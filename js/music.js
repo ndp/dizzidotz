27fd5832674e04a60bb452678f015d90332f1583
Andrew Peterson <andy@ndpsoftware.com>
2016-06-08 12:06:44 -0700

    Add a whole bunch of "build octave" fns.

--- js/music.js
+++ js/music.js
@@ -8,28 +8,140 @@ const majorThirdAbove = (ν) => ν * 5.0 / 4.0
 const perfectFourthAbove = (ν) => ν * 4.0 / 3.0
 const flatFiveAbove = (ν) => ν * Math.pow(semitone, 6)
 const perfectFifthAbove = (ν) => ν * 3.0 / 2.0
-const majorSixthAbove = (ν) => ν  * Math.pow(semitone, 9)
-const minorSeventhAbove = (ν) => ν  * Math.pow(semitone, 10)
-const majorSeventhAbove = (ν) => ν  * Math.pow(semitone, 11)
+const majorSixthAbove = (ν) => ν * Math.pow(semitone, 9)
+const minorSeventhAbove = (ν) => ν * Math.pow(semitone, 10)
+const majorSeventhAbove = (ν) => ν * Math.pow(semitone, 11)
 
 const floor = (x, d) => x - (x % d)
 const roundToEqualTempered = (f) => floor(f * 4000, semitone)
 
 
-// Nasty syntax for this, dudes...
-const octavesObj = {}
-octavesObj[Symbol.iterator] = function* () {
-  let a = 110
+function buildOctavesGenerator(lo, hi) {
+  const iterable = {}
+  iterable[Symbol.iterator] = function* () {
+    let a = lo
+    while (a < hi) {
+      yield a
+      a *= 2
+    }
+  }
+  return [...iterable]
+}
+
+function buildOctavesRecursive2a(lo, hi) {
+  return (function next(f) {
+    if (f * 2 < hi) {
+      return [f].concat(next(f * 2))
+    } else {
+      return [f]
+    }
+  })(lo)
+}
+
+function buildOctavesRecursive2b(lo, hi) {
+  return (function next(f) {
+    const n = f * 2
+    if (n < hi) {
+      return [f].concat(next(n))
+    } else {
+      return [f]
+    }
+  })(lo)
+}
+
+
+function buildOctavesRecursive2(lo, hi) {
+  return (function next(f) {
+    if (f < hi) {
+      const n = next(f*2)
+      return (n) ? [f].concat(n) : [f]
+    }
+  })(lo)
+}
+
+
+function buildOctavesGenerator2(lo, hi) {
+  const iterator = function* () {
+    let a = lo
+    while (a < hi) {
+      yield a
+      a *= 2
+    }
+  }
+  return [...iterator()]
+}
+
+function buildOctavesGenerator25(lo, hi) {
+  return [...(function* () {
+    let a = lo
+    while (a < hi) {
+      yield a
+      a *= 2
+    }
+  })()]
+}
+
+const iterateX2 = function* (x) {
   while (true) {
-    yield a
-    a *= 2;
-    if (a > 4000) return null
+    yield x
+    x *= 2
   }
 }
 
-const ν = {
-  octaves: [...octavesObj] // [110, 220, 440, 880, 1760, 1760 * 2]
+const takeWhile = function* (it, fn) {
+  for (x of it) {
+    if (fn(x))
+      yield x;
+    else
+      return
+  }
 }
+
+const whileLessThan = function (it, max) {
+  return takeWhile(it, (x) => x < max)
+}
+
+
+function buildOctavesGenerator3(lo, hi) {
+
+  const whileLessThan = function*(it, max) {
+    for (x of it) {
+      if (x < max)
+        yield x;
+      else
+        return
+    }
+  }
+
+  const c = whileLessThan(iterateX2(lo), hi)
+  return [...c]
+}
+
+
+function buildOctavesForLoop(lo, hi) {
+  const o = []
+  for (let f = lo; f < hi; f *= 2)
+    o.push(f)
+  return o
+}
+
+
+function buildOctavesRecursive(lo, hi) {
+  const o = [];
+  (function next(f) {
+    if (f < hi) {
+      o.push(f)
+      next(f * 2)
+    }
+  })(lo)
+  return o
+}
+
+const ν = {}
+ν.octaves = buildOctavesGenerator(110, 4000) // [110, 220, 440, 880, 1760, 3520]
+ν.octaves = buildOctavesForLoop(110, 4000)
+ν.octaves = buildOctavesRecursive(110, 4000)
+
 
 // Give a name to the scale, and provide any number of
 // functions to generate the notes above the tonic.
