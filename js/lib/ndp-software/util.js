af8ea7d3c31c66163474d6b65ba28d85734dc78c
Andrew Peterson <andy@ndpsoftware.com>
2016-06-21 13:45:44 -0700

    Add "within" for min/max range.

--- js/lib/ndp-software/util.js
+++ js/lib/ndp-software/util.js
@@ -3,3 +3,7 @@ const log = (x) => (y,z) => console.log(x, y, z)
 function precondition(x, msg) {
   if (!x) throw msg
 }
+
+function Math_within(x, min, max) {
+  return Math.min(max, Math.max(min, x))
+}
