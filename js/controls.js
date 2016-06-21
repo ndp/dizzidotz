9ffa070e95b2102438a79dd87fd078682725b55e
Andrew Peterson <andy@ndpsoftware.com>
2016-06-20 22:21:19 -0700

    Minor code cleanup, no-op.

--- js/controls.js
+++ js/controls.js
@@ -1,13 +1,13 @@
 
-const playPause = document.getElementById('play-pause')
-const playPauseClicks$ = Rx.Observable.fromEvent(playPause, 'click')
+const playPauseEl = document.getElementById('play-pause')
+const playPauseClicks$ = Rx.Observable.fromEvent(playPauseEl, 'click')
 
 const playPause$ = Rx.Observable
     .just(1)
     .merge(playPauseClicks$.scan((acc, e) => 1-acc, 1))
 
-const play$ = playPause$.filter((x) => x == 1)
-play$.subscribe(() => { playPause.className = 'playing' })
+const play$ = playPause$.filter(x => x == 1)
+play$.subscribe(() => { playPauseEl.className = 'playing' })
 
-const pause$ = playPause$.filter((x) => x == 0)
-pause$.subscribe(() => { playPause.className = 'paused' })
+const pause$ = playPause$.filter(x => x == 0)
+pause$.subscribe(() => { playPauseEl.className = 'paused' })
