357443a132a940ad004afc1958875c09569adc3b
Andrew Peterson <andy@ndpsoftware.com>
2016-06-30 16:47:03 -0700

    Add a piece of a donut.

--- js/dial.js
+++ js/dial.js
@@ -2,10 +2,10 @@ function newDial(dom, model$) {
 
   model$.subscribe(function(x) {
     const startAngle   = 0
-    const value        = x * 270 + startAngle
+    const value        = x * 350 + startAngle
     const tempoReading = dom.querySelector('.reading')
     tempoReading
-        .setAttribute('d', svgArc(50, 50, 45, startAngle, value))
+        .setAttribute('d', svgClippedArc(50, 50, 30, 45, startAngle, value))
 
   })
   // INTENT
@@ -15,10 +15,10 @@ function newDial(dom, model$) {
              return {x: e.layerX - 50, y: e.layerY - 50}
            })
       .map(pt => ptToVector(pt)[0])
+      .map(r=>r + Math.PI / 2)       // 0..2π
       .map(normalizeRadians)
-      .filter(r => r >= -Math.PI / 2)  // [-.5π..π]
-      .map(r=>r + Math.PI / 2)         // 0..3/2π
-      .map(r=>r * .66665 / Math.PI)    // normalize [0..1]
+      .map(r=> r < 0 ? r + 2 * Math.PI : r)
+      .map(r=>r * 0.5 / Math.PI)    // normalize [0..1]
       .subscribe(model$)
 }
 
--- js/lib/ndp-software/svg.js
+++ js/lib/ndp-software/svg.js
@@ -12,3 +12,23 @@ function svgArc(x, y, radius, startAngle, endAngle) {
   ].join(' ')
 }
 
+function svgClippedArc(x, y,
+                       innerRadius, outerRadius,
+                       startAngle, endAngle) {
+
+  const startOuter = polarToCartesian(x, y, outerRadius, endAngle)
+  const endOuter   = polarToCartesian(x, y, outerRadius, startAngle)
+  const startInner = polarToCartesian(x, y, innerRadius, endAngle)
+  const endInner   = polarToCartesian(x, y, innerRadius, startAngle)
+
+  const arcSweep = endAngle - startAngle <= 180 ? '0' : '1'
+
+  return [
+    'M', startOuter.x, startOuter.y,
+    'A', outerRadius, outerRadius, 0, arcSweep, 0, endOuter.x, endOuter.y,
+    'L', endInner.x, endInner.y,
+    'A', innerRadius, innerRadius, 0, arcSweep, 1, startInner.x, startInner.y,
+    'Z'
+  ].join(' ')
+}
+
