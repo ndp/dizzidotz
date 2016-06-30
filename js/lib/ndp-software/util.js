23baa7a38b51aa8dce3612ef4d3aa936753950d2
Andrew Peterson <andy@ndpsoftware.com>
2016-06-30 11:10:00 -0700

    Add linearScaleFns

--- js/lib/ndp-software/util.js
+++ js/lib/ndp-software/util.js
@@ -8,6 +8,48 @@ function Math_within(x, min, max) {
   return Math.min(max, Math.max(min, x))
 }
 
+/*
+ linearScaleFns: create functions that scale linearly
+ from [0..1] to a given range. Range can be:
+ * max
+ * min, max
+ * max, min (where scaling is inverted)
+ Returns two functions:
+ * first function (n) => x, where x is normalized 0..1, and x is the specified range
+ * the second function does the reverse scaling
+ */
+function linearScaleFns(minOrMax, max) {
+  let min
+
+  if (typeof(max) == 'undefined') {
+    [min,max] = [0, minOrMax]
+  } else {
+    min = minOrMax
+  }
+
+  let flipFn = (x) => x
+  if (min > max) {
+    [min, max] = [max, min]
+    flipFn = (x) => 1 - x
+  }
+
+  const range = max - min
+
+  const unscaleFn = (x) => Math_within(flipFn((x - min) / range), 0, 1)
+  const scaleFn   = (x) => Math_within((flipFn(x) * range) + min, min, max)
+  return [scaleFn, unscaleFn]
+}
+
+
 const localStorageKeys = []
-for (let i = 0; i < localStorage.length; i++)
-  localStorageKeys[i] = localStorage.key(i)
+if (typeof(localStorage) !== 'undefined') {
+  for (let i = 0; i < localStorage.length; i++)
+    localStorageKeys[i] = localStorage.key(i)
+}
+
+
+if (typeof(module) !== 'undefined') {
+  module.exports = {
+    linearScaleFns:     linearScaleFns
+  }
+}
