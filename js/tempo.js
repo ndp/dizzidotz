c3b2f8ecb058ce282c2c85685b9dd1f1249b17b2
Andrew Peterson <andy@ndpsoftware.com>
2016-06-30 11:14:44 -0700

    Use scaledBehaviorSubject

--- js/tempo.js
+++ js/tempo.js
@@ -2,9 +2,7 @@
 const msPerPeriod$ = new Rx.BehaviorSubject(2000)
 
 // 20050..50 => 0..1
-const normalizedTempo$ = mapBehaviorSubject(msPerPeriod$,
-                                            (x) => Math_within(1 - (x - 50) / 20000, 0, 1),
-                                            (x) => Math_within(((1 - x) * 20000) + 50, 50, 20000))
+const normalizedTempo$ = scaleBehaviorSubject(msPerPeriod$, 20000, 50)
 
 // VIEW
 newDial(document.getElementById('tempo-dial'), normalizedTempo$)
