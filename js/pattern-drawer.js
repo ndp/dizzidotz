a538cc8acbdfc18ecff277fdb8e3aa3d2faa1d42
Andrew Peterson <andy@ndpsoftware.com>
2016-07-04 13:33:03 -0700

    Show name in pattern drawer

--- js/pattern-drawer.js
+++ js/pattern-drawer.js
@@ -24,7 +24,23 @@ const renderPatterns = (patterns) => {
     const del     = document.createElement('A')
     del.innerHTML = document.getElementById('delete-icon').innerHTML
     del.className = DELETE_PATTERN_CLASS_NAME
+
+    const name     = document.createElement('span')
+    name.className = 'name'
+    name.innerHTML = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="200px" height="200px" viewBox="0 0 300 300" enable-background="new 0 0 300 300" stroke-widt="0">
+    <defs>
+        <path id="criclePath" d="M 150, 150 m -120, 0 a 120,120 0 0,1 240,0 a 120,120 0 0,1 -240,0 "/>
+    </defs>
+    <g>
+        <use xlink:href="#criclePath" fill="none"/>
+        <text fill="#fff" >
+            <textPath xlink:href="#criclePath">${pattern.name}</textPath>
+        </text>
+    </g>
+</svg>`
+    li.appendChild(name)
     li.appendChild(del)
+    li.appendChild(link)
 
     patternListElem.appendChild(li)
   })
@@ -45,9 +61,9 @@ patternsClicks$
     .filter((link) => link && link.className != DELETE_PATTERN_CLASS_NAME)
     .map((link) => link.closest('li'))
     .map(li => li.getAttribute('data-key'))
-    .withLatestFrom(patternStore$, (name, patterns) => patterns[name] )
+    .withLatestFrom(patternStore$, (name, patterns) => patterns[name])
     .map((pattern) => {
-           return { pattern, name: 'add pattern'}
+           return {pattern, name: 'add pattern'}
          })
     .subscribe(editorCmdBus$)
 
