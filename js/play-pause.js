568a5b22a9e1e36e54d918ed80579d9434e60f59
Andrew Peterson <andy@ndpsoftware.com>
2016-07-05 08:19:12 -0700

    persist play/pause state

--- js/play-pause.js
+++ js/play-pause.js
@@ -1,5 +1,8 @@
 // MODEL
-const playState$ = new Rx.BehaviorSubject('playing')
+const STORE_KEY  = 'play-pause'
+const playState$ = new Rx.BehaviorSubject(localStorage['STORE_KEY'] || 'playing')
+playState$
+    .subscribe(x => localStorage['STORE_KEY'] = x)
 
 playStateBus$ = newCmdBus$(playState$)
 playStateBus$.on('toggle', state => state == 'playing' ? 'paused' : 'playing')
