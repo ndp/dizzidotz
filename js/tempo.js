b0ac182fb690f814553d8375940a382d6a15a384
Andrew Peterson <andy@ndpsoftware.com>
2016-07-31 22:33:55 -0700

    Add keyboard handling for the tempo control

--- js/tempo.js
+++ js/tempo.js
@@ -1,12 +1,16 @@
 /*eslint-env browser */
 
 import {BehaviorSubject} from 'rxjs/BehaviorSubject'
-
 import 'rxjs/add/operator/mapTo'
+
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/observable/fromEvent'
 
 import {linearScaleFns, humanizeTempo} from './lib/ndp-software/util.js'
 import {mapBehaviorSubject} from './lib/ndp-software/map-behavior-subject.js'
 import {newDial} from './dial.js'
+
+import {newCmdBus$, logCmdBus} from './lib/ndp-software/rx-dux/cmdBus.js'
 
 
 // MODEL
@@ -15,6 +19,12 @@ const [unwrapFn, wrapFn] = linearScaleFns(20000, 50)
 const normalizedTempo$    = mapBehaviorSubject(msPerPeriod$,
                                                wrapFn,
                                                unwrapFn)
+
+const msPerPeriodCmd$ = newCmdBus$(msPerPeriod$)
+msPerPeriodCmd$.on('inc', state => state * 0.98)
+msPerPeriodCmd$.on('incXL', state => state * 0.92)
+msPerPeriodCmd$.on('dec', state => state * 1.02)
+msPerPeriodCmd$.on('decXL', state => state * 1.08)
 
 // VIEW
 const dial     = document.getElementById('tempo-dial')
@@ -33,3 +43,21 @@ msPerPeriod$
     .mapTo('value')
     .merge(preview$.map(x => x > 0 ? 'preview' : 'value'))
     .subscribe((cls) => textElem().classList[cls == 'preview' ? 'add' : 'remove']('preview'))
+
+
+// INTENT
+const keyDown$ = Observable.fromEvent(document, 'keydown')
+//keyDown$.subscribe((x) => console.log(x))
+
+keyDown$
+    .filter(e => e.key == 'ArrowUp')
+    .map(e => e.shiftKey ? 'incXL' : 'inc')
+    .subscribe(msPerPeriodCmd$)
+
+keyDown$
+    .filter(e => e.key == 'ArrowDown')
+    .map(e => e.shiftKey ? 'decXL' : 'dec')
+    .subscribe(msPerPeriodCmd$)
+
+logCmdBus//(msPerPeriodCmd$)
+
