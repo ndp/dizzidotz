04f10e9029d3c8b416f234d73a8a9f07b3dfb62a
Andrew Peterson <andy@ndpsoftware.com>
2016-10-03 22:23:44 -0700

    Add draggable method

---
+++ js/draggable.js
@@ -0,0 +1,103 @@
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/observable/combineLatest'
+import 'rxjs/add/observable/from'
+import 'rxjs/add/operator/combineLatest'
+import 'rxjs/add/operator/mergeMap'
+import 'rxjs/add/operator/concat'
+import 'rxjs/add/operator/startWith'
+import 'rxjs/add/operator/share'
+
+import { labelLog } from './lib/ndp-software/util.js'
+
+
+/**
+ * makeDraggable
+ *
+ * @param draggableCntr -- element in which to watch for drags
+ * @param mapDraggable -- given an element that the mouse is over, return an element that
+ *                        is draggable. Return `null` if not draggable.
+ * @param mapDropTarget -- given mouse position and the dragged element, return an element that can be dropped on, or null
+ * @param createOutlineEl -- create an element that is explicitly sized and can
+ *                be used to represent the outline of the object as it is dragged
+ * @returns {Observable} of commands reflecting the drag in process
+ */
+export const ACTION_DRAG_START = 'dragstart',
+ACTION_DRAG_MOVE               = 'drag',
+ACTION_DRAG_END                = 'dragend'
+
+
+export function makeDraggable({
+  draggableCntr,
+  mapDraggable,
+  mapDropTarget,
+  createOutlineEl}) {
+
+
+  const mouseup$   = Observable.fromEvent(document, 'mouseup')
+    .do(labelLog('mouseUp'))
+  const mousemove$ = Observable.fromEvent(document, 'mousemove')
+  //.do(labelLog('mouseMove'))
+  const mousedown$ = Observable.fromEvent(draggableCntr, 'mousedown')
+    .do(labelLog('mouseDown'))
+
+
+  return mousedown$
+    .filter(e => !!mapDraggable(e.target))
+    //.do(e => e.preventDefault())
+    //.do(e => e.stopPropagation())
+    .mergeMap(function(e) {
+                const el              = mapDraggable(e.target)
+                const start           = {x: e.clientX, y: e.clientY}
+                const outline         = createOutlineEl(el)
+                const originalTopLeft = {x: el.getClientRects()[0].left, y: el.getClientRects()[0].top}
+                console.log('CREATING OUTLINE')
+                const body            = document.getElementsByTagName('BODY')[0]
+                //body.insertBefore(outline, body.firstChild)
+                body.appendChild(outline)
+
+                function moveOutlineTo(offset) {
+                  //outline.style.transform = `translate(${offset.x}px, ${offset.y}px)`
+                  outline.style.left = `${originalTopLeft.x + offset.x}px`
+                  outline.style.top  = `${originalTopLeft.y + offset.y}px`
+                }
+
+                const dragAction$ = mousemove$
+                  .map((mme) => {
+                         return {
+                           name:   ACTION_DRAG_MOVE,
+                           dest:   mapDropTarget({
+                             x: mme.clientX,
+                             y: mme.clientY
+                           }, el),
+                           offset: {x: mme.clientX - start.x, y: mme.clientY - start.y},
+                                   el,
+                                   outline
+                         }
+                       })
+                  .do(action => moveOutlineTo(action.offset))
+                  .distinctUntilChanged(function(a, b) {
+                                          return a === b ||
+                                            (a !== null && b !== null && a.offset == b.offset)
+                                        })
+
+
+                const finishAction$ = Observable
+                  .from([{name: ACTION_DRAG_END}])
+                  .do(() => outline.parentNode.removeChild(outline))
+                  .do(() => console.log('DELETING OUTLINE')
+                )
+
+                const action$ = dragAction$
+                  .takeUntil(mouseup$)
+                  .concat(finishAction$)
+                  .withLatestFrom(dragAction$, (action, dragAction)=> {
+                                    if (action.name !== ACTION_DRAG_END) return action
+                                    return Object.assign({}, dragAction, action)
+                                  })
+                  .startWith({name: ACTION_DRAG_START, el: el})
+                return action$
+              })
+    .share()
+
+
+}
