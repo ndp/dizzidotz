2786f2c9b6c21f63d14b4f572fd18954965ca823
Andrew Peterson <andy@ndpsoftware.com>
2021-04-24 15:31:40 -0700

    one last straggler behavior subject

--- src/noise.js
+++ src/noise.js
@@ -1,4 +1,4 @@
-import { Subject }                      from 'rxjs/Subject'
+import { Subject }                      from 'rxjs'
 import { currentTonality$, tonalities } from './tonality.js'
 import * as Tone from 'tone'
 
